112a0d6e97acd131b85899fad35cfe73cb628ee0
Fábio Henrique Carmona <100398695+fhcarmona@users.noreply.github.com>
2024-12-09 23:26:34 -0300

    Connecting Signal Code

--- scripts/sprite_2d.gd
+++ scripts/sprite_2d.gd
@@ -6,6 +6,10 @@ var angular_speed: float = PI
 func _init() -> void:
 	print("Hello world!")
 
+func _ready() -> void:
+	var timer = get_node("Timer")
+	timer.timeout.connect(_on_timer_timeout)
+
 func _process(delta: float) -> void:
 	rotation += angular_speed * delta
 	var velocity = Vector2.UP.rotated(rotation) * speed
@@ -13,3 +17,6 @@ func _process(delta: float) -> void:
 
 func _on_button_pressed() -> void:
 	set_process(not is_processing())
+	
+func _on_timer_timeout():
+	visible = not visible
